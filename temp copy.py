af78092e6d4c7d368caebc99d0fb87e962834c47
rohithreddykota <rohithreddykota@gmail.com>
2024-04-22 16:04:49 -0400

    update

--- temp copy.py
+++ temp copy.py
@@ -1,48 +1,58 @@
 
 info_1_1_32 = {
+    'execution_time': 22363.56542421826,  
+    'total_cpus': 1,
+    'total_threads': 1,
+    'rmse': 4.981931287950711,  
+    'r2': 0.91250622039369627,  
+    'cross_val_scores': [0.90, 0.91, 0.91, 0.90, 0.89],
+    'average_score': 0.90 
+}
+
+info_CPU_1_ChunkSize_32 = {
     'execution_time': 24363.7652242183685303,  
     'total_cpus': 1,
-    'Threads_per_cpu': 1,
+    'total_threads': 1,
     'rmse': 4.751921288990311,  
     'r2': 0.91250622039369627,  
     'cross_val_scores': [0.93, 0.92, 0.94, 0.91, 0.95],
     'average_score': 0.90  
 }
 
-info_8_1_64 = {
+info_CPU_4_ChunkSize_32 = {
     'execution_time': 13381.88261210929893230,  
     'total_cpus': 4,
-    'Threads_per_cpu': 1,
+    'total_threads': 1,
     'rmse': 4.512109228899021,  
     'r2': 0.90393696211927,  
     'cross_val_scores': [0.92, 0.93, 0.94, 0.93, 0.92],
     'average_score': 0.91  
 }
 
-info_8_4_64 = {
+info_CPU_8_ChunkSize_64 = {
     'execution_time': 7921.2190242100372,  
-    'total_cpus': 4,
-    'Threads_per_cpu': 1,
+    'total_cpus': 8,
+    'total_threads': 1,
     'rmse': 4.289372638203,  
     'r2': 0.9292302838492,  
     'cross_val_scores': [0.92, 0.92, 0.93, 0.91, 0.90],
     'average_score': 0.91  
 }
 
-info_16_4_64 = {
+info_CPU_16_ChunkSize_64 = {
     'execution_time': 3142.222963269374,  
     'total_cpus': 16,
-    'Threads_per_cpu': 1,
+    'total_threads': 1,
     'rmse': 3.59836283720,  
     'r2': 0.9197473847,  
     'cross_val_scores': [0.90, 0.91, 0.92, 0.93, 0.91],
     'average_score': 0.92  
 }
 
-info_28_4_64 = {
+info_CPU_28_ChunkSize_64 = {
     'execution_time': 9750.416662630553,  
     'total_cpus': 28,
-    'Threads_per_cpu': 1,
+    'total_threads': 1,
     'rmse': 3.88372648392,  
     'r2': 0.893847393745,  
     'cross_val_scores': [0.89, 0.90, 0.90, 0.89, 0.92],
